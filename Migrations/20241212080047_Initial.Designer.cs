7854049df0495272f4f70f89fb992d1614fbb827
DirtyDanPutnam <daniel.putnam@ndus.edu>
2024-12-12 02:59:54 -0600

    Only Shelter can add event, Only Show logged in shelter's events, and address filled in on edit.

--- Migrations/20241212080047_Initial.Designer.cs
+++ Migrations/20241212080047_Initial.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CanineConnect.Migrations
 {
     [DbContext(typeof(CanineConnectContext))]
-    [Migration("20241211214332_Initial")]
+    [Migration("20241212080047_Initial")]
     partial class Initial
     {
         /// <inheritdoc />
