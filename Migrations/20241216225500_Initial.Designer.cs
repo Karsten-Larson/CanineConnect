97b966d6cf5c002e5d45b967a6f70c215bf0ba10
Karsten-Larson <karsten.larson.1@gmail.com>
2024-12-16 17:07:54 -0600

    Got dog application working

--- Migrations/20241216225500_Initial.Designer.cs
+++ Migrations/20241216225500_Initial.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CanineConnect.Migrations
 {
     [DbContext(typeof(CanineConnectContext))]
-    [Migration("20241216223712_initial")]
-    partial class initial
+    [Migration("20241216225500_Initial")]
+    partial class Initial
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
